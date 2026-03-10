49bf4cde147b4c90ef77b3876c98f04cba7d5687
iamarya842 <mdm441785@gmail.com>
2026-03-10 14:25:36 +0530

    Enhance bot functionality with message deletion and link generation

--- bot.py
+++ bot.py
@@ -1,23 +1,43 @@
 import telebot
+import time
+import threading
 
 TOKEN = "8153911184:AAFeWlRNxbf-yD_oWyMCuWVMorZR6dRngpo"
-CHANNEL_ID = -1003789323635   # apna channel id
+BOT_USERNAME = "Ghopghop842_bot"
 
 bot = telebot.TeleBot(TOKEN)
 
+
+def delete_after(chat_id, msg_id):
+    time.sleep(1200)  # 20 minutes
+    try:
+        bot.delete_message(chat_id, msg_id)
+    except:
+        pass
+
+
 @bot.message_handler(commands=['start'])
 def start(message):
-    text = message.text.split()
 
-    if len(text) > 1:
+    args = message.text.split()
+
+    if len(args) > 1:
         try:
-            post_id = int(text[1])
+            data = args[1].split("_")
 
-            bot.copy_message(
+            channel_id = int(data[0])
+            post_id = int(data[1])
+
+            msg = bot.copy_message(
                 message.chat.id,
-                CHANNEL_ID,
+                channel_id,
                 post_id
             )
+
+            threading.Thread(
+                target=delete_after,
+                args=(message.chat.id, msg.message_id)
+            ).start()
 
         except:
             bot.send_message(message.chat.id, "⚠️ Post nahi mila")
@@ -25,26 +45,28 @@ def start(message):
     else:
         bot.send_message(message.chat.id, "🤖 Bot Online Hai\n\nChannel post link bhejo")
 
-@bot.message_handler(func=lambda message: True)
-def generate_link(message):
+
+@bot.message_handler(func=lambda m: True)
+def link_gen(message):
 
     if "t.me/c/" in message.text:
-
         try:
-            post_id = message.text.split("/")[-1]
+            parts = message.text.split("/")
+            channel = parts[-2]
+            post = parts[-1]
 
-            link = f"https://t.me/Ghopghop842_bot?start={post_id}"
+            channel_id = "-100" + channel
 
-            bot.send_message(
-                message.chat.id,
-                f"✅ Bot Link:\n\n{link}"
-            )
+            link = f"https://t.me/{BOT_USERNAME}?start={channel_id}_{post}"
+
+            bot.send_message(message.chat.id, f"✅ Bot Link:\n\n{link}")
 
         except:
             bot.send_message(message.chat.id, "⚠️ Link galat hai")
 
     else:
         bot.send_message(message.chat.id, "❌ Channel post link bhejo")
+
 
 print("Bot started...")
 bot.infinity_polling()
